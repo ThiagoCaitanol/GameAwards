0693a11493e8a5593d8a951dc69d6841b2b2a1ca
Thiago <thiago.caitanov@gmail.com>
2022-12-12 22:16:41 -0300

    Inserindo metodo pacth na requisicao

--- src/main/java/me/api/apigameawards/controller/games/GameController.java
+++ src/main/java/me/api/apigameawards/controller/games/GameController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -40,6 +41,12 @@ public class GameController extends BaseRestController{
 	public ResponseEntity<Game> insert(@RequestBody Game game) {
 		businessLayer.insert(game);
 		return ResponseEntity.ok(game);
+	}
+	
+	@PatchMapping("games/{id}/vote")
+	public ResponseEntity<Void> votee(@PathVariable Long id) {
+		this.businessLayer.vote(id);
+		return ResponseEntity.ok().build();
 	}
 	
 	@PutMapping("games/{id}")
